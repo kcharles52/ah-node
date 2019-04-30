5211d9188de37709e9394a97a118bf91089dfc53
Kato Charles <kcharles52@gmail.com>
2019-04-30 11:53:49 +0300

    chore(database): setup mongoDB connection (#6)
    - add setup for mongoDB connection
    
    [Finishes #165671795]

--- src/__tests__/server.spec.js
+++ src/__tests__/server.spec.js
@@ -1,5 +1,19 @@
+import request from 'supertest'
+import { app } from '../server'
+
 describe('API dummy test', () => {
   test('setup test', () => {
     expect('test').toBe('test')
+  })
+})
+
+describe('make reqests to the API', () => {
+  it('should respond with json data', (done) => {
+    request(app)
+      .get('api/data')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+    done()
   })
 })
